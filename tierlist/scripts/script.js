b28677f6e6fb342cd0c471ddff07d9e349511693
jonathanyu1825 <112720627+jonathanyu1825@users.noreply.github.com>
2025-10-01 03:13:38 -0400

    testing https

--- tierlist/scripts/script.js
+++ tierlist/scripts/script.js
@@ -113,7 +113,8 @@ document.addEventListener("DOMContentLoaded", () => {
 
   async function performSearch(query) {
     // const url = `http://localhost:3000/api/search/${encodeURIComponent(query)}`;
-    const url = `http://34.138.234.164:80/api/search/${encodeURIComponent(query)}`;
+    const url = `https://34.138.234.164:80/api/search/${encodeURIComponent(query)}`;
+    // const url = `http://34.138.234.164:80/api/search/${encodeURIComponent(query)}`;
     try {
       const response = await fetch(url);
       if (!response.ok) {
